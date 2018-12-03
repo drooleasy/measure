4f0bb8baa1bebc1fe356be4f1ea8a12339170819
Julien Droulez <julien.droulez@free.fr>
2018-12-03 11:50:48 +0100

    add add() and mult()

--- measure.js
+++ measure.js
@@ -2,25 +2,33 @@ function measure(str){
 	var match = str.match(/^\s*(\d*\.?\d+)(\S*)\s*$/);
 	var _val = match && parseFloat(match[1]) || 0;
 	var _unit = match && match[2] || "";
-	var f = function (){
+	var f = function measure_instance(){
 		if(arguments.length){
 			_val = arguments[0];
 		}
 		return _val + _unit;
 	}
-	f.unit = function(){
+	f.unit = function measure_unit(){
 		if(arguments.length){
 			_unit = arguments[0];
 			return f;
 		}
 		return _unit;
 	}
-	f.value = function(){
+	f.value = function measure_value(){
 		if(arguments.length){
 			_val = arguments[0];
 			return f;
 		}
 		return _val;
 	}
-	return f;	
+	f.add = function measure_add(v){
+		_val += v;
+		return f;
+	}
+	f.mult = function measure_mult(v){
+		_val *= v;
+		return f;
+	}
+	return f;
 }
